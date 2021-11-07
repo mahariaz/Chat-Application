6c978c776810b1f22c10ded1378f752d0d624f86
Mubashir <mahariaz96@gmail.com>
2021-11-07 23:46:16 +0500

    phone verification error of security

--- app/src/main/java/com/mahariaz/i181652_180681/signUp.java
+++ app/src/main/java/com/mahariaz/i181652_180681/signUp.java
@@ -28,21 +28,21 @@ public class signUp extends AppCompatActivity {
     String email_signup,password_signup,conf_pass;
     Button signup_button;
     boolean empty_field=true,pass_matches=false;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mAuth = FirebaseAuth.getInstance();
+
+
+
+
         signup_button=findViewById(R.id.signup_button);
-
-
-
         signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 email_field_signup = findViewById(R.id.email_s);
                 password_field_signup = findViewById(R.id.pass);
@@ -50,35 +50,9 @@ public class signUp extends AppCompatActivity {
                 email_signup = email_field_signup.getText().toString();
                 password_signup = password_field_signup.getText().toString();
                 conf_pass = confirm_field.getText().toString();
-                System.out.println(email_signup);
-                System.out.println(password_signup);
-
-                mAuth.createUserWithEmailAndPassword(email_signup,password_signup)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()){
-                                    System.out.println("Uptil here");
-                                    User user=new User(email_signup,password_signup);
-                                    FirebaseDatabase.getInstance().getReference("User")
-                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()){
-                                                Toast.makeText(signUp.this,"S",Toast.LENGTH_LONG).show();
-
-                                                //
-
-                                            }
-                                        }
-                                    });
-
-
-                                }
-                            }
-                        });
-
+                Intent intent=new Intent(signUp.this,VerifyPhoneNo.class);
+                intent.putExtra("phoneNo",email_signup);
+                startActivity(intent);
 
 
 
